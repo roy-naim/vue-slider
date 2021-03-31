2e4b02c22c6b18ee63be0bd29e0df5bc0756f624
Roy <b.roynaim@gmail.com>
2021-03-31 16:32:15 +0200

    sliding activated

---
+++ main.js
@@ -0,0 +1,30 @@
+var app = new Vue(
+  {
+  el: '#root',
+  data: {
+    index: 0,
+    imgSrc: '',
+    arrayImgs: [
+      'https://static.dezeen.com/uploads/2019/02/UTS-Business-School-by-Frank-Gehry_dezeen_sq-a.jpg',
+      'https://www.inexhibit.com/wp-content/uploads/2019/12/Daniel-Libeskind-MO-Museum-Vilnius-Lithuania-exterior-6-870x580.jpg',
+      'https://i.pinimg.com/originals/1c/62/13/1c62137909e0773beea00927c60ec26d.jpg',
+    ]
+  },
+
+  methods: {
+    next: function () {
+      if (this.index < this.arrayImgs.length - 1) {
+        this.index++;
+      } else {
+        this.index = 0;
+      }
+    },
+    prev: function () {
+      if (this.index > 0) {
+        this.index--;
+      } else {
+        this.index = this.arrayImgs.length - 1;
+      }
+    }
+  }
+});
